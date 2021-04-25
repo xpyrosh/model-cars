4455a3530cf8241b5e72483f2cc9778140639e60
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-04-24 20:00:42 -0400

    installed bootswatch theme, installed react-bootstrap and started to build header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello Car Guy</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Model Karz</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
